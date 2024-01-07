6f13e253ea7e7c59d7e362378842f53df111b695
AleVera96 <alejandrovera.av96@gmail.com>
2024-01-07 18:56:21 -0300

    Agregando router y controllers

---
+++ src/controllers/indexController.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+//const json = fs.readFileSync(path.join(__dirname,"../database/products.json"),"utf-8")
+//const products = JSON.parse(json);
+
+const indexController = {
+    home: (req,res)=>{
+        res.render("index")
+    }
+}
+
+module.exports = indexController;
