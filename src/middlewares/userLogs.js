65e1befbdf82cd0c34d477aa3aed814fa6124992
DAVELEON23 <davidaarondeleon@gmail.com>
2024-01-07 20:40:07 -0300

    Se trabajo en carpeta middleware para dejar registro de usuarios

--- src/middlewares/userLogs.js
+++ src/middlewares/userLogs.js
@@ -1,7 +1,8 @@
 const fs = require ("fs");
 
 function login (req,res,next) {
-    fs.readFileSync("userLogs.txt", "Ingresaste a la pagina" + req.url) //para dejar registro en el archivo txt
+    fs.readFileSync("userLogs.txt", "Ingresaste a la pagina" + req.url+'\n') //para dejar registro en el archivo txt
+    next();
 }
 
 module.exports = login 
